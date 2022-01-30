af61140ca0d8947f0612be4c83c160b3627a12e2
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-01-30 07:04:25 +0530

    Added API router

---
+++ pages/api/[tokenId].js
@@ -0,0 +1,11 @@
+export default function handler(req,res) {
+  const tokenId = req.query.tokenId;
+  const image_url = "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+
+  //This api is sending back metadata for a crypto dev
+  res.status(200).json({
+    name:"CryptoDev #" + tokenId,
+    description : "Crypto Dev is a collection of developer in crypto",
+    image: image_url + tokenId + ".svg"
+  });
+}
